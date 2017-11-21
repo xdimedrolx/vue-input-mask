8d2bca92e42ef5273ef4914a04b8772ec8db6918
xdimedrolx <xdimedrolx@yandex.ru>
2017-11-21 13:40:51 +0300

    fix building

--- build/release.sh
+++ build/release.sh
@@ -4,8 +4,7 @@ npm run build
 npm version patch
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 git add *;
-git commit -m "Commit message"
-
+git commit -m "Release $version"
 
 if [ "$version" != "" ]; then
     git tag -a "v$version" -m "`git log -1 --format=%s`"
