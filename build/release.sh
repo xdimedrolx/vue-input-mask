44905b101719e2c7b66906531fe3be3bcfb5a0c1
xdimedrolx <xdimedrolx@yandex.ru>
2017-11-21 13:37:39 +0300

    Auto increment version of npm and git tagging

--- build/release.sh
+++ build/release.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 npm run build
-npm publish
+
+npm version patch
+version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+git add *;
+git commit -m "Commit message"
+
+
+if [ "$version" != "" ]; then
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, v$version"
+    git push --tags
+    npm publish
+fi
+
 node build/del.js
