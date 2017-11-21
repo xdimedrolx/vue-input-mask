eb3d759e58ee44e7206a13a3fa82223b0360b86c
xdimedrolx <xdimedrolx@yandex.ru>
2017-11-21 14:10:59 +0300

    fix release cmd

--- build/release.sh
+++ build/release.sh
@@ -1,16 +1,26 @@
-#!/bin/sh
+#!/bin/bash -e
+
+printf "Which type of release would you like bump or minor or patch?"
+read versionType
+
+newVersion=`npm version --no-git-tag-version $versionType`
+echo "Preparing new version ${newVersion}"
+
+# echo "Type enter when you have updated the CHANGELOG.md for ${newVersion}"
+# read proceed
+
+echo "Running npm build"
 npm run build
 
-npm version patch
-version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
-git add *;
-git commit -m "Release $version"
+echo "Type enter to commit and tag"
+read proceed
+git add .
+git commit -m "Updating to version ${newVersion}"
+git tag -a ${newVersion} -m "Updating to version ${newVersion}"
+git push --follow-tags
 
-if [ "$version" != "" ]; then
-    git tag -a "v$version" -m "`git log -1 --format=%s`"
-    echo "Created a new tag, v$version"
-    git push --tags
-    npm publish
-fi
+echo "Type enter publish to npm"
+read proceed
+npm publish
 
 node build/del.js
