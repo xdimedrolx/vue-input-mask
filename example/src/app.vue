4d6b7b032a5418e53aebf999e25cd259ea66f8f0
xdimedrolx <xdimedrolx@yandex.ru>
2018-04-12 13:07:45 +0300

    build

--- example/src/app.vue
+++ example/src/app.vue
@@ -6,11 +6,17 @@
     <input-mask v-model="phoneTimeout" mask="+7 (999) 999-99-99"></input-mask>
     <p>{{ phoneTimeout }}</p>
 
+    <input-mask v-model="nullPassportTimeout" mask="99999 999999"></input-mask>
+    <p>{{ nullPassportTimeout }}</p>
+
     <input-mask v-model="passport" mask="99999 999999"></input-mask>
     <p>{{ passport }}</p>
 
     <input-mask disabled v-model="passportDisabled" mask="99999 999999"></input-mask>
     <p>{{ passportDisabled }}</p>
+
+    <input-mask v-model="defaultValue" mask="+7 (999) 999-99-99" default-value="+79996665544"></input-mask>
+    <p>{{ defaultValue }}</p>
   </div>
 </template>
 
@@ -26,16 +32,19 @@ export default {
 
   data() {
     return {
+      defaultValue: null,
       phone: '+79996665544',
       phoneTimeout: null,
       passport: '99999 123456',
-      passportDisabled: '99999 123456'
+      passportDisabled: '99999 123456',
+      nullPassportTimeout: null
     }
   },
 
   mounted () {
     setTimeout(() => {
       this.phoneTimeout = '+79996665544'
+      this.nullPassportTimeout = null
     }, 700)
   },
 }
